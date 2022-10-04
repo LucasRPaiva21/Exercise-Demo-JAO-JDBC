755661b65f18efb7d4b143b170817e417f9f8907
Lucas Paiva <lucas.ramalhopaiva@hotmail.com>
2022-10-04 20:34:27 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,17 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		Seller seller = new Seller(21, "Lucas", "lucas.ramalhopaiva@hotmail.com", new Date(), 8384.56, obj);
+		System.out.println(seller);
 
 	}
 
